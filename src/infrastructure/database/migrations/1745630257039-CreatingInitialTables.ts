901457654f50a79bd4afb43182ad055bab3ce797
Lucas Dogo <lucasdogo01@gmail.com>
2025-04-26 23:32:09 -0300

    refactor: update app module and docker configuration, add initial database migration

--- src/infrastructure/database/migrations/1745630257039-CreatingInitialTables.ts
+++ src/infrastructure/database/migrations/1745630257039-CreatingInitialTables.ts
@@ -1,19 +1,265 @@
-// import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
 
-// export class CreatingInitialTables1745630257039 implements MigrationInterface {
+export class CreatingInitialTables1745630257039 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
 
-//     public async up(queryRunner: QueryRunner): Promise<void> {
-//         await queryRunner.createTable( new Table{
-//             name: 'users',
-//             columns: [
-//                 {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'phone',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'isActive',
+            type: 'boolean',
+            default: true,
+            isNullable: false,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'addressId',
+            type: 'uuid',
+            isNullable: false,
+          },
+          {
+            name: 'roleId',
+            type: 'uuid',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
 
-//                 }
-//             ]
-//         })
-//     }
+    await queryRunner.createTable(
+      new Table({
+        name: 'role',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'name',
+            type: 'enum',
+            enum: ['ADMIN', 'USER', 'GUEST'],
+            isNullable: false,
+          },
+        ],
+      }),
+    );
 
-//     public async down(queryRunner: QueryRunner): Promise<void> {
-//     }
+    await queryRunner.createTable(
+      new Table({
+        name: 'rolePermissions',
+        columns: [
+          {
+            name: 'roleId',
+            type: 'uuid',
+            isNullable: false,
+          },
+          {
+            name: 'permissionId',
+            type: 'uuid',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
 
-// }
+    await queryRunner.createTable(
+      new Table({
+        name: 'permissions',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'permissionName',
+            type: 'varchar',
+            isNullable: false,
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'address',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'cep',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'country',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'state',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'city',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'district',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'street',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'number',
+            type: 'integer',
+            isNullable: false,
+          },
+          {
+            name: 'complement',
+            type: 'varchar',
+            isNullable: true,
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'users',
+      new TableForeignKey({
+        columnNames: ['addressId'],
+        referencedTableName: 'address',
+        referencedColumnNames: ['id'],
+        onDelete: 'CASCADE',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'users',
+      new TableForeignKey({
+        columnNames: ['roleId'],
+        referencedTableName: 'role',
+        referencedColumnNames: ['id'],
+        onDelete: 'CASCADE',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'rolePermissions',
+      new TableForeignKey({
+        columnNames: ['roleId'],
+        referencedTableName: 'role',
+        referencedColumnNames: ['id'],
+        onDelete: 'CASCADE',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'rolePermissions',
+      new TableForeignKey({
+        columnNames: ['permissionId'],
+        referencedTableName: 'permissions',
+        referencedColumnNames: ['id'],
+        onDelete: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const tableUsers = await queryRunner.getTable('users');
+    if (tableUsers) {
+      const foreignKeyAddress = tableUsers.foreignKeys.find(
+        (fk) => fk.columnNames.indexOf('addressId') !== -1,
+      );
+      const foreignKeyRole = tableUsers.foreignKeys.find(
+        (fk) => fk.columnNames.indexOf('roleId') !== -1,
+      );
+      if (foreignKeyAddress) {
+        await queryRunner.dropForeignKey('users', foreignKeyAddress);
+      }
+      if (foreignKeyRole) {
+        await queryRunner.dropForeignKey('users', foreignKeyRole);
+      }
+    }
+
+    const tableRolePermissions = await queryRunner.getTable('rolePermissions');
+    if (tableRolePermissions) {
+      const foreignKeyRolePermissionsRole =
+        tableRolePermissions.foreignKeys.find(
+          (fk) => fk.columnNames.indexOf('roleId') !== -1,
+        );
+      const foreignKeyRolePermissionsPermission =
+        tableRolePermissions.foreignKeys.find(
+          (fk) => fk.columnNames.indexOf('permissionId') !== -1,
+        );
+      if (foreignKeyRolePermissionsRole) {
+        await queryRunner.dropForeignKey(
+          'rolePermissions',
+          foreignKeyRolePermissionsRole,
+        );
+      }
+      if (foreignKeyRolePermissionsPermission) {
+        await queryRunner.dropForeignKey(
+          'rolePermissions',
+          foreignKeyRolePermissionsPermission,
+        );
+      }
+    }
+
+    await queryRunner.dropTable('rolePermissions');
+    await queryRunner.dropTable('permissions');
+    await queryRunner.dropTable('address');
+    await queryRunner.dropTable('role');
+    await queryRunner.dropTable('users');
+  }
+}
---
+++ src/infrastructure/database/migrations/1745719029716-InitUser.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
+export class InsertInitialData1745630257040 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const password = await bcrypt.hash('Admin@123', 10);
+
+    await queryRunner.query(`
+      INSERT INTO "role" ("id", "name") VALUES
+      ('11111111-1111-1111-1111-111111111111', 'ADMIN'),
+      ('11111111-1111-1111-1111-111111111112', 'USER'),
+      ('11111111-1111-1111-1111-111111111113', 'GUEST');
+    `);
+
+    await queryRunner.query(`
+        INSERT INTO "permissions" ("id", "permissionName") VALUES
+        ('11111111-1111-1111-1111-111111111114', 'READ'),
+        ('11111111-1111-1111-1111-111111111115', 'WRITE'),
+        ('11111111-1111-1111-1111-111111111116', 'DELETE');
+      `);
+
+    await queryRunner.query(`
+      INSERT INTO "address" ("id", "cep", "country", "state", "city", "district", "street", "number", "complement") VALUES
+      ('11111111-1111-1111-1111-111111111117', '12345678', 'Brasil', 'SP', 'São Paulo', 'Centro', 'Rua A', 100, 'Apto 101');
+    `);
+
+    await queryRunner.query(`
+      INSERT INTO "users" ("id", "name", "email", "phone", "isActive", "password", "addressId", "roleId") VALUES
+      ('11111111-1111-1111-1111-111111111118', 'João da Silva', 'joao@exemplo.com', '999999999', true, '${password}', 
+      (SELECT "id" FROM "address" WHERE "cep" = '12345678' LIMIT 1), 
+      (SELECT "id" FROM "role" WHERE "name" = 'ADMIN' LIMIT 1));
+    `);
+
+    await queryRunner.query(`
+      INSERT INTO "rolePermissions" ("roleId", "permissionId") VALUES
+      ((SELECT "id" FROM "role" WHERE "name" = 'ADMIN' LIMIT 1), 
+      (SELECT "id" FROM "permissions" WHERE "permissionName" = 'READ' LIMIT 1)),
+      ((SELECT "id" FROM "role" WHERE "name" = 'ADMIN' LIMIT 1), 
+      (SELECT "id" FROM "permissions" WHERE "permissionName" = 'WRITE' LIMIT 1)),
+      ((SELECT "id" FROM "role" WHERE "name" = 'USER' LIMIT 1), 
+      (SELECT "id" FROM "permissions" WHERE "permissionName" = 'READ' LIMIT 1));
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('DELETE FROM "rolePermissions";');
+    await queryRunner.query('DELETE FROM "users";');
+    await queryRunner.query('DELETE FROM "address";');
+    await queryRunner.query('DELETE FROM "permissions";');
+    await queryRunner.query('DELETE FROM "role";');
+  }
+}
